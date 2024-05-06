292b988976499d18244261400de24bc522df0ad7
nboos <nboos@DESKTOP-4M2RMIC>
2024-05-05 21:04:41 -0400

    - Narrowed search of images to "dog photo" and "cat photo", "single x photo" was giving me lots of images that contained both the animal and a human which was odd. - changed item_tfms from Resize to RandomResizedCrop - added batch_tfms=aug_transforms() for default data augmentations - saved/exported final model to file 'export.pkl'

--- catvdog.py
+++ catvdog.py
@@ -7,7 +7,7 @@ from fastai.vision.all import *
 from fastdownload import download_url
 from time import sleep
 
-def search_images(term, max_images=50):
+def search_images(term, max_images=200):
     print(f"Searching for '{term}'")
     return (L(search_images_ddg(term, max_images=max_images)))
 
@@ -15,28 +15,37 @@ if __name__ == '__main__':
     searches = 'cat', 'dog'
     path = Path('images/cat_or_dog')
 
-    #for o in searches:
-    #    dest = (path/o)
-    #    dest.mkdir(exist_ok=True, parents=True)
-    #    download_images(dest, urls=search_images(f'single {o} photo'))
-    #    sleep(10)
-    #    resize_images(path/o, max_size=400, dest=path/o)
+#    for o in searches:
+#        dest = (path/o)
+#        dest.mkdir(exist_ok=True, parents=True)
+#        download_images(dest, urls=search_images(f'{o} photo'))
+#        sleep(10)
+#        resize_images(path/o, max_size=400, dest=path/o)
 
-    failed = verify_images(get_image_files(path))
-    failed.map(Path.unlink)
+#    failed = verify_images(get_image_files(path))
+#    failed.map(Path.unlink)
 
-    dls = DataBlock(
-        blocks=(ImageBlock, CategoryBlock),
-        get_items=get_image_files,
-        splitter=RandomSplitter(valid_pct=0.2, seed=42),
-        get_y=parent_label,
-        item_tfms=[Resize(192, method='squish')]
-    ).dataloaders(path, bs=32)
+#    dls = DataBlock(
+#        blocks=(ImageBlock, CategoryBlock),
+#        get_items=get_image_files,
+#        splitter=RandomSplitter(valid_pct=0.2, seed=42),
+#        get_y=parent_label,
+#        item_tfms=[RandomResizedCrop(224, min_scale=0.5)],
+#        batch_tfms=aug_transforms()
+#    ).dataloaders(path, bs=32)
 
 
-    learn = vision_learner(dls, resnet18, metrics=error_rate)
-    learn.fine_tune(3)
+#    learn = vision_learner(dls, resnet18, metrics=error_rate)
+#    learn.fine_tune(4)
 
-    cat_or_dog,_,probs = learn.predict(PILImage.create('images/dog.jpg'))
-    print(f"This is a: {cat_or_dog}.")
-    print(f"Probability: {probs[0]:.4f}")
+    learn_inf = load_learner('export.pkl')
+    learn_inf.predict('images/dog.jpg')
+
+    print()
+#    interp = ClassificationInterpretation.from_learner(learn)
+#    interp.plot_confusion_matrix()
+
+    #cat_or_dog,_,probs = learn.predict(PILImage.create('images/cat.jpg'))
+    #print(f"This is a: {cat_or_dog}.")
+    #print(f"Probability: {probs[0]:.4f}")
+
